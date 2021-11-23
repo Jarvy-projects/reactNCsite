1f3c82425c9247bb7a8d41d24170b29c31615f80
Jarvy Gomez Rosas <jarvygomezrosas@gmail.com>
2021-11-23 00:43:29 -0800

    Presentational and Container Components 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
+
 import './App.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES
-        };
-    }
-
     render() {
         return (
             <div className="App">
